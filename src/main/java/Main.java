53c1fa4f588319cbecd0a98a17fa6929cc61b3f8
Emschy <emilschmitt9@gmail.com>
2025-10-28 20:42:07 +0100

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+//CCCCCCCCCCCCCCCCCCCCCAAAAAAAAAA
     public static void main(String[] args) {
         Factory factory = new Factory();
 
